6976ebe7062cd8e3c2f7ce01de0019f48352a4e4
iliass <iliassaek.dev@gmail.com>
2025-01-24 08:41:31 +0100

    feat: turkey can quack

--- src/main/java/adapter/TurkeyAdapter.java
+++ src/main/java/adapter/TurkeyAdapter.java
@@ -1,8 +1,14 @@
 package adapter;
 
 public class TurkeyAdapter implements Duck{
+    Turkey turkey;
+
+    public TurkeyAdapter() {
+        turkey = new WildTurkey();
+    }
+
     @Override
     public String quack() {
-        return Constants.GOBBLING;
+        return turkey.gobble();
     }
 }
