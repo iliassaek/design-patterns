130c11dc5ade1ab67273ea5afed82657a3980664
iliass <iliassaek.dev@gmail.com>
2025-02-10 08:38:43 +0100

    chore: refactor to switch case

--- src/main/java/simplefactory/Constants.java
+++ src/main/java/simplefactory/Constants.java
@@ -2,9 +2,9 @@ package simplefactory;
 
 public class Constants {
 
-    static String CHEESE_TYPE = "Cheese";
-    static String CHEESE_DESCRIPTION = "cheese and a lot of mozzarilla";
+    static final String CHEESE_TYPE = "Cheese";
+    static final String CHEESE_DESCRIPTION = "cheese and a lot of mozzarilla";
 
-    static String VEGETARIAN_TYPE = "vegetarian";
-    static String VEGETARIAN_DESCRIPTION = "carrots and tomatoes";
+    static final String VEGETARIAN_TYPE = "vegetarian";
+    static final String VEGETARIAN_DESCRIPTION = "carrots and tomatoes";
 }
--- src/main/java/simplefactory/ProductFactory.java
+++ src/main/java/simplefactory/ProductFactory.java
@@ -1,10 +1,16 @@
 package simplefactory;
 
+import static simplefactory.Constants.CHEESE_TYPE;
 import static simplefactory.Constants.VEGETARIAN_TYPE;
 
 public class ProductFactory {
     static Pizza createPizza(String type){
-        if(type.equals(VEGETARIAN_TYPE)) return new VegetarianPizza();
-        return new CheesePizza();
+
+        Pizza pizza = switch (type){
+            case VEGETARIAN_TYPE -> new VegetarianPizza();
+            case CHEESE_TYPE -> new CheesePizza();
+            default -> throw new IllegalArgumentException();
+        };
+        return pizza;
     }
 }
